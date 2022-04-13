273efc552b8cfb4090af49059a954c75166c08b6
Rustlesscobra <franciscolealhenriques@usp.br>
2022-04-13 18:16:40 -0300

    Implementação da caixa de diálogo
    Caixa de diálogo com sprite do personagem adicionada

---
+++ PROJECT LOGOS/Assets/Scripts/Trigger_dialogos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger_dialogos : MonoBehaviour
+{ 
+    public Image dialogue_box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue_box.gameObject.SetActive(true);
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
