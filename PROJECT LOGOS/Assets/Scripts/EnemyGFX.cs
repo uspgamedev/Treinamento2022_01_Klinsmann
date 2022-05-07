7e7f4343db771151b740a770ef52d03ac1ff077a
Rustlesscobra <franciscolealhenriques@usp.br>
2022-05-07 14:15:53 -0300

    animação do inimigo implementada

--- PROJECT LOGOS/Assets/Scripts/EnemyGFX.cs
+++ PROJECT LOGOS/Assets/Scripts/EnemyGFX.cs
@@ -6,17 +6,18 @@ using Pathfinding;
 public class EnemyGFX : MonoBehaviour
 {
     public AIPath aipath;
+    public Animator animator;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        if(aipath.desiredVelocity.x <= -0.01f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+        
+        movement.x = aipath.desiredVelocity.x;
+        movement.y = aipath.desiredVelocity.y;
 
-        } else if(aipath.desiredVelocity.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
+        animator.SetFloat("X", movement.x);
+        animator.SetFloat("Y", movement.y);
+        animator.SetFloat("enemy_speed", movement.sqrMagnitude);
     }
 }
